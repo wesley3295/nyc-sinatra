c64b40f735f7f4b1c796bc60e2f0b4bf94f256ff
Wesley <wspencer3295@yahoo.com>
2020-12-23 16:52:05 -0700

    commit tree 408db00c0b1f523593076ad7ab43258a4d90c7c9

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,26 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures/new' do 
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params['figure'])
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 end
